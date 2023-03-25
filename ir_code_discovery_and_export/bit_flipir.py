1279ee6ba9c6cae40a1be9f9154566279be03134
Sean Yem <github@sean1983.com>
2023-03-25 11:11:31 +0000

    Duplicate Lines
    Removed duplicate serial lines

--- ir_code_discovery_and_export/bit_flipir.py
+++ ir_code_discovery_and_export/bit_flipir.py
@@ -27,13 +27,6 @@ ARDUINO_BAUD_RATE = 115200
 
 arduino = serial.Serial(port=ARDUINO_SERIAL_PORT, baudrate=ARDUINO_BAUD_RATE, timeout=.1)
 
-# Which serial port the Arduino is connected to. You can find this with the Arduino IDE or follow these instructions:
-# https://www.mathworks.com/help/supportpkg/arduinoio/ug/find-arduino-port-on-windows-mac-and-linux.html
-ARDUINO_SERIAL_PORT = "COM5"
-
-# Baud rate of the serial connection set up on the Arduino. It is 115200 in the included sketches.
-ARDUINO_BAUD_RATE = 115200
-
 ############################################################
 layout = [[sg.Text("", key="scan_text")],
           [sg.Column([[sg.Button(STARTING_BITS[bit_num], pad=(0, 0), key=f"bit_{bit_num}",
