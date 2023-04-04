5c8889f8f306fb596a3e38da8d98bcc4e53fa770
Sean Yem <github@sean1983.com>
2023-04-04 06:07:16 +0100

    Tail Codes are working now.
    The 24bits are Tail Codes are working now,
    
    It still needs some work to polish up the error codes, and copy and paste for the tails.
    
    We can push and pull for now as i will leave it as file name _tail until clean.

--- python_tools/bit_flipir_tail.py
+++ python_tools/bit_flipir_tail.py
@@ -10,10 +10,11 @@ import config as cfg
 # values in a list, where they can click on one to "flip" that bit and automatically send the new data via IR
 
 # What to start the window's bit list at
-STARTING_BITS = [1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1]
-TAIL_START_BITS = [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+STARTING_BITS = [1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1]
+TAIL_START_BITS = [0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1]
 # Make this bigger or smaller to change the size of everything in the GUI
-SIZE_SCALING = 2.3
+SIZE_SCALING = 1.8
 
 # How long to wait between sends after pressing "Resend 10x" (seconds).
 RESEND_DELAY = 0.075
@@ -23,37 +24,37 @@ tailcode_mode = False
 arduino = serial.Serial(port=cfg.ARDUINO_SERIAL_PORT, baudrate=cfg.ARDUINO_BAUD_RATE, timeout=.1)
 
 ############################################################
-tailM = [sg.Column([[sg.Button(TAIL_START_BITS[tail_bit_num], pad=(0, 0), key=f"bit_{tail_bit_num}", button_color="green" if TAIL_START_BITS[tail_bit_num] == 1 else "red")], [sg.Text(tail_bit_num, font='Helvitica 6')]], element_justification='c', pad=(0, 0))  for tail_bit_num in range(len(TAIL_START_BITS))],
+tailM = [sg.Column([[sg.Button(TAIL_START_BITS[tail_bit_num], pad=(0, 0), key=f"bit_{tail_bit_num+len(STARTING_BITS)}", button_color="green" if TAIL_START_BITS[tail_bit_num] == 1 else "red")], [sg.Text(tail_bit_num+len(STARTING_BITS), font='Helvitica 6')]], element_justification='c', pad=(0, 0)) for tail_bit_num in range(len(TAIL_START_BITS))],[sg.Text("", key="tail_text")],
 
-layout =  [[sg.Text("", key="scan_text")],
-          [sg.Column([[sg.Button(STARTING_BITS[bit_num], pad=(0, 0), key=f"bit_{bit_num}", button_color="green" if STARTING_BITS[bit_num] == 1 else "red")], [sg.Text(bit_num, font='Helvitica 6')]], element_justification='c', pad=(0, 0))  for bit_num in range(len(STARTING_BITS))],
-          [sg.Column(tailM, key="ShowTailMenu", visible = False)],
+layout = [[sg.Text("", key="scan_text")],
+         [sg.Column([[sg.Button(STARTING_BITS[bit_num], pad=(0, 0), key=f"bit_{bit_num}", button_color="green" if STARTING_BITS[bit_num] == 1 else "red")], [sg.Text(bit_num, font='Helvitica 6')]], element_justification='c', pad=(0, 0)) for bit_num in range(len(STARTING_BITS))],
+         [sg.Push(),sg.Column(tailM, key="ShowTailMenu", visible = False)],
+
           [sg.Button("Resend", key="resend"), sg.Button("Resend 10x", key="resend_10x"), sg.Button("Use Tailcode", key="use_tailcode"),
-           sg.Push(), sg.Button("Copy to clipboard", key="copy"), sg.Button("Paste from clipboard", key="paste")],
-          [sg.Text("", key="error_text", font='Helvitica 11 bold')],
-          [sg.Exit()]]
+          sg.Push(), sg.Button("Copy to clipboard", key="copy"), sg.Button("Paste from clipboard", key="paste")],
+         [sg.Text("", key="error_text", font='Helvitica 11 bold')],
+         [sg.Exit()]]
 
 
 window = sg.Window('BitFlipIR', layout, scaling=SIZE_SCALING)
 
 
 def send_effect_from_bits(effect_bits):
-
     arduino_string_ver = to_arduino_string(effect_bits)
     arduino.write(bytes(arduino_string_ver, 'utf-8'))
 
-    # print(f"Sent effect: {','.join([str(bit) for bit in effect_bits])} arduino string: {arduino_string_ver}")
+    print(f"Sent effect: {','.join([str(bit) for bit in effect_bits])} arduino string: {arduino_string_ver}")
 def update_button_colors(window):
     try:
         [window[f"bit_{bit_num}"].update(button_color="green" if window[f"bit_{bit_num}"].get_text() == "1" else "red")
          for bit_num in range(len(STARTING_BITS))]
-        [window[f"bit_{tail_bit_num}"].update(button_color="green" if window[f"bit_{tail_bit_num}"].get_text() == "1" else "red")
+        [window[f"bit_{tail_bit_num+len(STARTING_BITS)}"].update(button_color="green" if window[f"bit_{tail_bit_num+len(STARTING_BITS)}"].get_text() == "1" else "red")
          for tail_bit_num in range(len(TAIL_START_BITS))]
     except:
-        time.sleep(0.01)
+        time.sleep(0.0075)
         [window[f"bit_{bit_num}"].update(button_color="green" if window[f"bit_{bit_num}"].get_text() == "1" else "red")
          for bit_num in range(len(STARTING_BITS))]
-        [window[f"bit_{tail_bit_num}"].update(button_color="green" if window[f"bit_{tail_bit_num}"].get_text() == "1" else "red")
+        [window[f"bit_{tail_bit_num+len(STARTING_BITS)}"].update(button_color="green" if window[f"bit_{tail_bit_num+len(STARTING_BITS)}"].get_text() == "1" else "red")
          for tail_bit_num in range(len(TAIL_START_BITS))]
 
 while True:
@@ -75,18 +76,23 @@ while True:
     elif event == "resend" :
         print("Will resend")  # Continue without changing bits
     elif event == "resend_10x":
+        window[f"resend_10x"].disabled = True
+
         print("Will resend 10x")
         for _ in range(9):  # 9 because we will also resend one time later
             new_selected_bits = [int(window[f"bit_{bit_num}"].get_text()) for bit_num in range(len(STARTING_BITS))]
-            new_tail_bits = [int(window[f"bit_{tail_bit_num}"].get_text()) for tail_bit_num in range(len(TAIL_START_BITS))]
+            new_tail_bits = [int(window[f"bit_{tail_bit_num+len(STARTING_BITS)}"].get_text()) for tail_bit_num in range(len(TAIL_START_BITS))]
             try:
-                if tailcode_mode == True:
-                    send_effect_from_bits(new_tail_bits + new_selected_bits)
+                if tailcode_mode:
+                    send_effect_from_bits(new_selected_bits + new_tail_bits)
                     time.sleep(RESEND_DELAY)
                 else:
                     send_effect_from_bits(new_selected_bits)
             except:
                 pass  # Error will still be shown from before
+        time.sleep(1.5)
+        window[f"resend_10x"].disabled=False
+        continue
     elif event == "copy":
         clipboard.copy(str([int(window[f"bit_{bit_num}"].get_text()) for bit_num in range(len(STARTING_BITS))]))
         continue
@@ -110,11 +116,11 @@ while True:
         window[event].update('1', button_color="darkgreen")
 
     new_selected_bits = [int(window[f"bit_{bit_num}"].get_text()) for bit_num in range(len(STARTING_BITS))]
-    new_tail_bits = [int(window[f"bit_{tail_bit_num}"].get_text()) for tail_bit_num in range(len(TAIL_START_BITS))]
+    new_tail_bits = [int(window[f"bit_{tail_bit_num+len(STARTING_BITS)}"].get_text()) for tail_bit_num in range(len(TAIL_START_BITS))]
     try:
         if tailcode_mode == True:
             send_effect_from_bits(new_selected_bits + new_tail_bits)
-            send_effect_from_bits(new_selected_bits)
+            window["error_text"].update(error_msg)
         else:
             send_effect_from_bits(new_selected_bits)
     except:
@@ -129,5 +135,6 @@ while True:
     else:
         window["error_text"].update("")
         window["scan_text"].update(f"Sent: {new_selected_bits}")
+        window["tail_text"].update(f"Sent: {new_tail_bits}")
 
 window.close()
