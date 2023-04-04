84b58a690dc3386a606393b68892d7a3c5767f38
zacharesmer <zachresmer@gmail.com>
2023-04-04 19:37:26 -0400

    move rle decoding to conversion funcs

--- python_tools/pixmob_conversion_funcs.py
+++ python_tools/pixmob_conversion_funcs.py
@@ -20,6 +20,7 @@ def bits_to_run_lengths_pulses(bit_list):
     # convert from a list of 1s and 0s to run length by number of pulses
     # Example: [1, 1, 1, 0, 0, 0, 0, 1] -> [3, 4, 1]
     # TODO: Throw an exception if the bit list isn't just ones and zeroes?
+    # TODO: Combine this with the other bit to run length function and let this one be the case where pulse length is 1
     run_lengths = []
     # groupby returns groups of adjacent matching things
     for _, group in itertools.groupby(bit_list):
@@ -33,6 +34,29 @@ def bits_to_run_lengths_microseconds(bit_list, pulse_length=cfg.PULSE_LENGTH):
     # Example: [1, 0, 0, 0, 0, 1, 1] -> [1, 4, 2] -> [700, 2800, 1400]
     # Note that in this example, cfg.PULSE_LENGTH is 700
     return [pulses * pulse_length for pulses in bits_to_run_lengths_pulses(bit_list)]
+
+
+def run_lengths_to_bits(run_length_list, pulse_length=cfg.PULSE_LENGTH, acceptable_error=1):
+    # Convert list of run lengths to 1s and 0s based on the pulse length in the kwarg
+    # Resulting lists always start with 1
+    # If acceptable_error is set to something less than 1, an exception will be raised if we find a remainder
+    # greater than that fraction of the pulse length.
+    # Example [700, 2100, 1400] -> [1, 0, 0, 0, 1, 1]
+    # Additional example with error check: If acceptable_error is set to .1, [700, 1900, 1400] would cause an error
+    # because 1900 is 200 off from the closest multiple of 700, which is larger than .1 * 700 = 70
+    bit_list = []
+    bit = 1
+    for run_length in run_length_list:
+        difference = run_length % pulse_length
+        # check for too high and too low
+        epsilon = min(difference, abs(pulse_length - difference))
+        if epsilon > acceptable_error * pulse_length:
+            raise ValueError(f"Error too large: {run_length} is {epsilon} away from nearest possible value, must be within {acceptable_error*pulse_length}")
+
+        pulses = int(round(run_length / pulse_length))
+        bit_list += [bit] * pulses
+        bit = (bit + 1) % 2
+    return bit_list
 
 
 def bits_to_arduino_string(bit_list):
--- python_tools/tests/test_pixmob_conversion_funcs.py
+++ python_tools/tests/test_pixmob_conversion_funcs.py
@@ -35,6 +35,22 @@ class Test(TestCase):
         with self.assertRaises(ValueError):
             funcs.bits_to_arduino_string(input)
 
+    def test_run_lengths_to_bit_list_flipper_values(self):
+        input = [1407, 2069, 727, 1354, 1389, 2774, 729, 2036, 1414, 1352, 726, 2067, 1436]
+        expected_output = [1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1]
+        self.assertEqual(funcs.run_lengths_to_bits(input, pulse_length=694, acceptable_error=1), expected_output)
+
+    def test_run_lengths_to_bit_list_just_pulses(self):
+        input = [2, 3, 1, 2, 2, 4, 1, 3, 2, 2, 1, 3, 2]
+        expected_output = [1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1]
+        self.assertEqual(funcs.run_lengths_to_bits(input, pulse_length=1, acceptable_error=1), expected_output)
+
+    def test_run_lengths_to_bit_list_raises_ValueError_if_data_too_far_off(self):
+        input = [1407, 2069, 727, 1354, 1389, 2774, 729, 2036, 1414, 1352, 726, 2067, 1436]
+        with self.assertRaises(ValueError):
+            funcs.run_lengths_to_bits(input, pulse_length=694, acceptable_error=.01)
+
+
 
 if __name__ == '__main__':
     unittest.main()
